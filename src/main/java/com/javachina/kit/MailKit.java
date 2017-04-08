f43de2dd928f740101473da36a672fe7ff3ba293
yhpdev <278291362@qq.com>
2017-04-08 17:24:24 +0800

    特殊字符报错
    executorService.execute(new Runnable() {

--- src/main/java/com/javachina/kit/MailKit.java
+++ src/main/java/com/javachina/kit/MailKit.java
@@ -11,41 +11,45 @@ import java.util.concurrent.Executors;
  */
 public class MailKit {
 
-    private static ExecutorService executorService = Executors.newFixedThreadPool(3);
+	private static ExecutorService executorService = Executors
+			.newFixedThreadPool(3);
 
-    public static void sendForgot(String login_name, String email, String code) {
+	public static void sendForgot(String login_name, String email, String code) {
 
-    }
+	}
 
-    public static void sendSignup(String login_name, String to_addr, String code) {
-        String url = Constant.SITE_URL + "/active/" + code;
-        String content = "您的激活链接是：<a href='" + url + "'>" + url + "</a> 点击链接激活账号！";
-        send(login_name + ", 欢迎你加入" + Constant.MAIL_USERNAME, to_addr, content);
-    }
+	public static void sendSignup(String login_name, String to_addr, String code) {
+		String url = Constant.SITE_URL + "/active/" + code;
+		String content = "您的激活链接是：<a href='" + url + "'>" + url
+				+ "</a> 点击链接激活账号！";
+		send(login_name + ", 欢迎你加入" + Constant.MAIL_USERNAME, to_addr, content);
+	}
 
-    public static void send(final String subject, final String to_addr, final String content) {
-        executorService.execute(() -> {
-            try {
-                // Create the email message
-                HtmlEmail email = new HtmlEmail();
-                email.setHostName(Constant.MAIL_HOST);
-                email.addTo(to_addr);
-                //email.setStartTLSEnabled(true);
-                email.setFrom(Constant.MAIL_USER, Constant.MAIL_USERNAME);
-                email.setAuthentication(Constant.MAIL_USER, Constant.MAIL_PASS);
-                email.setCharset("UTF-8");
+	public static void send(final String subject, final String to_addr,
+			final String content) {
+		executorService.execute(new Runnable() {
+			@Override
+			public void run() {
+				try {
+					// Create the email message
+					HtmlEmail email = new HtmlEmail();
+					email.setHostName(Constant.MAIL_HOST);
+					email.addTo(to_addr);
+					// email.setStartTLSEnabled(true);
+					email.setFrom(Constant.MAIL_USER, Constant.MAIL_USERNAME);
+					email.setAuthentication(Constant.MAIL_USER,
+							Constant.MAIL_PASS);
+					email.setCharset("UTF-8");
 
-                email.setSubject(subject);
-                // set the html message
-                email.setHtmlMsg(content);
-                // send the email
-                email.send();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        });
-
-    }
-
-
+					email.setSubject(subject);
+					// set the html message
+					email.setHtmlMsg(content);
+					// send the email
+					email.send();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
 }
